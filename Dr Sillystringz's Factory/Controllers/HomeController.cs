a7dcec37e2f2f7bed6c2909f96dd04a42f5bad91
ESC18 <ponderosaprintingus@gmail.com>
2023-08-04 11:11:11 -0700

    Add project files.

---
+++ Dr Sillystringz's Factory/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Dr_Sillystringz_s_Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Dr_Sillystringz_s_Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Dr Sillystringz's Factory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dr_Sillystringz_s_Factory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
